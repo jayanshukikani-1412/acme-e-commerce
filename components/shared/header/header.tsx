a3a76e9119781b677e3d387017bb1845b271e441
jayanshukikani-1412 <jayanshukikani14@gmail.com>
2025-11-10 17:01:29 +0530

    Feat: Add Header and Footer components to enhance layout structure in RootLayout.

---
+++ components/shared/header/header.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b ">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href={"/"} className="flex-start">
+            <Image
+              src={"/images/logo.svg"}
+              alt="logo"
+              width={48}
+              height={48}
+              priority
+            />
+            <span className="hidden font-bold text-2xl ml-3 lg:block">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+
+        <div className="space-x-2">
+          <Button asChild variant={"ghost"}>
+            <Link href={"/cart"}>
+              <ShoppingCart className="size-6" />
+              <span>Cart</span>
+            </Link>
+          </Button>
+          <Button asChild variant={"ghost"}>
+            <Link href={"/sign-in"}>
+              <UserIcon className="size-6" />
+              <span>Sign In</span>
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
